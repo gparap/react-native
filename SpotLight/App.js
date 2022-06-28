8baf88127af1833b910d1064a8d877a5ef357c6b
gparap <gparap@protonmail.com>
2022-06-28 12:50:42 +0200

    Initial commit (SpotLight)

---
+++ SpotLight/App.js
@@ -0,0 +1,24 @@
+/**
+ * SpotLight React Native App
+ * Created by gparap (c) 2022
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>SpotLight App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
